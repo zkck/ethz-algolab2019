82a38912df5cee8ff82a6b4cc7201b2a6b26e95b
Zak Cook <zakcook4@gmail.com>
2019-11-12 01:09:46 +0100

    Fully working version for Suez

--- potw9/main.cpp
+++ potw9/main.cpp
@@ -71,45 +71,93 @@ int main(int argc, char const *argv[])
                     lp.set_b(row++, 2 * std::abs(y1 - y2));
                 }
             }
-
-            // constraints against fixed nails
-            // for (size_t j = 0; j < m; j++)
-            // {
-            //     IT x2, y2;
-            //     std::tie(x2, y2) = fixed_nails[j];
-
-            //     double slope_now = x2 - x1 == 0 
-            //         ? (y2 - y1) / ((double) (x2 - x1))
-            //         : std::numeric_limits<double>::infinity();
-
-            //     if (std::abs(slope_now) <= slope)
-            //     {
-            //         if (x1 < x2)
-            //         {
-            //             lp.set_a(i, row, w);
-            //             lp.set_b(row++, 2 * (x2 - x1) - w);
-            //         }
-            //         else
-            //         {
-            //             lp.set_a(i, row, w);
-            //             lp.set_b(row++, 2 * (x1 - x2) - w);
-            //         }
-            //     }
-            //     else
-            //     {
-            //         if (y1 < y2)
-            //         {
-            //             lp.set_a(i, row, h);
-            //             lp.set_b(row++, 2 * (y2 - y1) - h);
-            //         }
-            //         else
-            //         {
-            //             lp.set_a(i, row, h);
-            //             lp.set_b(row++, 2 * (y1 - y2) - h);
-            //         }
-            //     }
-            // }
         }
+
+        for (size_t i = 0; i < n; i++)
+        {
+            IT x1, y1; std::tie(x1, y1) = free_nails[i];
+
+            int left  = -1;
+            int right = -1;
+
+            IT left_x2, right_x2;
+
+            for (size_t j = 0; j < m; j++)
+            {
+                IT x2, y2; std::tie(x2, y2) = fixed_nails[j];
+                double slope_now = (y2 - y1) / (double) (x2 - x1);
+                if (std::abs(slope_now) <= slope) {
+                    if (x2 < x1 && (left < 0 || left_x2 < x2)) {
+                        left = j;
+                        left_x2 = x2;
+                    }
+                    if (x1 < x2 && (right < 0 || x2 < right_x2)) {
+                        right = j;
+                        right_x2 = x2;
+                    }
+                }
+            }
+
+            // now add the constraints
+
+            // left
+            if (left >= 0)
+            {
+                lp.set_a(i, row, w);
+                lp.set_b(row++, 2 * (x1 - left_x2) - w);
+            }
+
+            // right
+            if (right >= 0)
+            {
+                lp.set_a(i, row, w);
+                lp.set_b(row++, 2 * (right_x2 - x1) - w);
+            }
+        }
+
+
+        for (size_t i = 0; i < n; i++)
+        {
+            IT x1, y1; std::tie(x1, y1) = free_nails[i];
+
+            int above  = -1;
+            int below = -1;
+
+            IT above_y2, below_y2;
+
+            for (size_t j = 0; j < m; j++)
+            {
+                IT x2, y2; std::tie(x2, y2) = fixed_nails[j];
+                double slope_now = (y2 - y1) / (double) (x2 - x1);
+                if (std::abs(slope_now) > slope) {
+                    if (y2 < y1 && (below < 0 || below_y2 < y2)) {
+                        below = j;
+                        below_y2 = y2;
+                    }
+                    if (y1 < y2 && (above < 0 || y2 < above_y2)) {
+                        above = j;
+                        above_y2 = y2;
+                    }
+                }
+            }
+
+            // now add the constraints
+
+            // above
+            if (above >= 0)
+            {
+                lp.set_a(i, row, h);
+                lp.set_b(row++, 2 * (above_y2 - y1) - h);
+            }
+
+            // below
+            if (below >= 0)
+            {
+                lp.set_a(i, row, h);
+                lp.set_b(row++, 2 * (y1 - below_y2) - h);
+            }
+        }
+        
         
 
         for (size_t i = 0; i < n; i++)

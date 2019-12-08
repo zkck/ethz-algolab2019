ba7f70d7757e0be7ea8ec82004b944c1defc8406
Zak Cook <zakcook4@gmail.com>
2019-12-08 21:17:58 +0100

    Week 12 files

---
+++ week12/magician_coin/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int num_tests; std::cin >> num_tests;
+    while (num_tests-- > 0)
+    {
+        int n, k, m; std::cin >> n >> k >> m;
+
+        int p[n];
+
+        for (size_t i = 0; i < n; i++)
+        {
+            std::cin >> p[i];
+
+            
+        }
+        
+    }
+    
+    return 0;
+}

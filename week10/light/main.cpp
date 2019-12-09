cc95c733e8a7fbadcbb5e7a183864612682d31c6
Zak Cook <zakcook4@gmail.com>
2019-12-09 22:35:03 +0100

    Working solution for light the stage

--- week10/light/main.cpp
+++ week10/light/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Delaunay_triangulation_2.h>
@@ -42,11 +44,17 @@ int main(int argc, char const *argv[])
 
         Delaunay T;
         T.insert(lamps.begin(), lamps.end());
+
+        std::vector<size_t> winners;
+        size_t winning_round = 0; // did not work with int -1
         
         for (size_t i = 0; i < m; i++)
         {
             Point p = participants[i];
-            Point l = T.nearest_vertex(p)->point();
+            Delaunay::Vertex_handle l = T.nearest_vertex(p);
+
+            // radius in which a lamp must be to light up the participant
+            size_t limit = (radii[i] + h) * (radii[i] + h);
             
             // first group of testsets:
             //   - at least one participant is still in the game after
@@ -55,10 +63,61 @@ int main(int argc, char const *argv[])
             // which means that the winners are those that have not been
             // hit
 
-            if (CGAL::squared_distance(p, l) >= (radii[i] + h) * (radii[i] + h))
-                std::cout << i << " ";
+            // if (CGAL::squared_distance(p, l->point()) >= limit)
+            //     std::cout << i << " ";
+
+            // second group of testsets:
+            //   - no info
+            //
+            // try bfs from nearest vertex?
+
+            // we are minimizing the eliminating round
+            size_t eliminating_round = n;
+
+            std::vector<bool> visited(n, false);
+            std::queue<Delaunay::Vertex_handle> Q;
+
+            if (CGAL::squared_distance(p, l->point()) < limit) {
+                visited[l->info()] = true;
+                Q.push(l);
+            }
+
+            while (!Q.empty())
+            {
+                const Delaunay::Vertex_handle u = Q.front();
+                Q.pop();
+
+                // check if eliminating round
+                if (u->info() < eliminating_round) {
+                    eliminating_round = u->info();
+                    if (eliminating_round < winning_round)
+                        break;
+                }
+
+                Delaunay::Vertex_circulator c = T.incident_vertices(u);
+                do
+                {
+                    if (T.is_infinite(c)) continue;
+                    bool in_limit = CGAL::squared_distance(p, c->point()) < limit;
+                    if (in_limit && !visited[c->info()])
+                    {
+                        visited[c->info()] = true;
+                        Q.push(c);
+                    }
+                } while (++c != T.incident_vertices(u));
+            }
+
+            if (eliminating_round >= winning_round) {
+                if (eliminating_round > winning_round) {
+                    winning_round = eliminating_round;
+                    winners.clear();
+                }
+                winners.push_back(i);
+            }
         }
 
+        for (size_t w : winners)
+            std::cout << w << " ";
         std::cout << std::endl;
         
         

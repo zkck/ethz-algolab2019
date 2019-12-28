9e11c2cdae4415cda249cf131c7143e561f82ace
Zak Cook <zakcook4@gmail.com>
2019-12-28 21:03:31 +0100

    New approach by creating a tree using a bfs

--- potw4/main.cpp
+++ potw4/main.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
-// costs of reparation of each city
+// memo
+std::vector<int> memo;
+
+// as per tree modeling of the problem
+std::vector<std::vector<int>> children;
+
+// costs of rebuilding a city
 std::vector<int> costs;
 
-// calculated minimum costs
-std::vector<int> min_costs;
-
-// children of each city, as per tree modeling of the problem
-std::vector<std::vector<size_t>> children;
-
-int selecting_cost(size_t i) {
-    // check memo
-    if (min_costs[i] > 0)
-        return min_costs[i];
-    int cost = costs[i];
+int selecting_cost(int i) {
+    // if (memo[i] > 0) return memo[i];
+    int result = costs[i];
     for (int c1 : children[i])
         for (int c2 : children[c1])
-            cost += selecting_cost(c2);
-    min_costs[i] = cost;
-    return cost;
-
+            result += selecting_cost(c2);
+    // memo[i] = result;
+    return result;
 }
 
 int main(int argc, char const *argv[])
@@ -29,40 +27,58 @@ int main(int argc, char const *argv[])
     std::cin >> num_tests;
     while (num_tests-- > 0)
     {
+        // get number of cities
         int n; std::cin >> n;
 
-        children.clear();
-        min_costs.clear();
-
-        children.resize(n);
-        min_costs.resize(n, -1);
+        // adjacency information
+        std::vector<int> adj[n];
 
         for (size_t stage = 0; stage < n - 1; stage++)
         {
             int i, j; std::cin >> i >> j;
-            if (i < j)
-                children[i].push_back(j);
-            else
-                children[j].push_back(i);
+            adj[i].push_back(j);
+            adj[j].push_back(i);
         }
 
         costs.clear();
+        costs.resize(n);
+        
         for (size_t i = 0; i < n; i++)
         {
             int c; std::cin >> c;
             costs.push_back(c);
         }
-        
 
-        // compare between selecting the 0-node or its children
+
+
+        // step 1, build the tree
+
+        children.clear();
+        children.resize(n);
+        
+        std::queue<int> Q;
+        std::vector<bool> visited(n, false);
+        Q.push(0);
+        visited[0] = true;
+        while (!Q.empty()) {
+            int v = Q.front(); Q.pop();
+            for (int u : adj[v]) {
+                if (!visited[u]) {
+                    children[v].push_back(u);
+                    Q.push(u);
+                    visited[u] = true;
+                }
+            }
+        }
+
+        // step 2
         int selecting = selecting_cost(0);
         int not_selecting = 0;
         for (int c : children[0])
             not_selecting += selecting_cost(c);
 
-        std::cout
-            << (selecting > not_selecting ? not_selecting : selecting)
-            << std::endl;
+        std::cout << std::min(selecting, not_selecting) << std::endl;
+        
     }
     
     return 0;

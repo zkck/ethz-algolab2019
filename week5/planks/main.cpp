0a58c5338acb018eb3b60178066a8e05b3aff3b5
Zak Cook <zakcook4@gmail.com>
2020-01-02 15:50:33 +0100

    Start on Planks

---
+++ week5/planks/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int num_tests; std::cin >> num_tests;
+    while (num_tests-- > 0)
+    {
+        int n; std::cin >> n;
+        int l[n];
+        for (size_t i = 0; i < n; i++)
+        {
+            std::cin >> l[i];
+        }
+
+        
+        
+    }
+    
+    return 0;
+}

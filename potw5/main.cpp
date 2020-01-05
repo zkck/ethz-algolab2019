1ff398292f1cbe4765bb217d9a8a9cdca43c5934
Zak Cook <zakcook4@gmail.com>
2020-01-05 14:04:41 +0100

    Trying things out

--- potw5/main.cpp
+++ potw5/main.cpp
@@ -6,7 +6,7 @@
 typedef CGAL::Exact_predicates_inexact_constructions_kernel IK;
 typedef CGAL::Exact_predicates_exact_constructions_kernel   EK;
 
-int compare_bikers(IK::Ray_2 &b1, IK::Ray_2 &b2)
+int compare_bikers(IK::Segment_2 &b1, IK::Segment_2 &b2)
 {
     return b1.source().y() > b2.source().y();
 }
@@ -17,12 +17,12 @@ int main(int argc, char const *argv[])
     while (num_tests-- > 0)
     {
         int n; std::cin >> n;
-        std::vector<IK::Ray_2> bikers;
+        std::vector<IK::Segment_2> bikers;
 
         for (size_t i = 0; i < n; i++)
         {
             long y0, x1, y1; std::cin >> y0 >> x1 >> y1;
-            IK::Ray_2 biker(IK::Point_2(0, y0), IK::Point_2(x1, y1));
+            IK::Segment_2 biker(IK::Point_2(0, y0), IK::Point_2(x1, y1));
             bikers.push_back(biker);
         }
 
@@ -30,7 +30,7 @@ int main(int argc, char const *argv[])
 
         std::vector<IK::FT> ydiffs;
         for (auto &b : bikers) {
-            IK::FT ydiff = b.point(1).y() - b.source().y();
+            IK::FT ydiff = b.target().y() - b.source().y();
             ydiffs.push_back(ydiff);
             std::cout << "  " << b << " is going " << ydiff << std::endl;
         }
@@ -57,33 +57,20 @@ int main(int argc, char const *argv[])
             for (int j : range) {
                 if (is_cut_off) break;
                 if (CGAL::do_intersect(bikers[i], bikers[j])) {
-                    auto o = CGAL::intersection(bikers[i], bikers[j]);
-                    if (IK::Point_2 *op = boost::get<IK::Point_2>(&*o)) {
-                        std::cout << "  " << *op << std::endl;
-                        auto comp = CGAL::compare_distance_to_point(
-                            bikers[i].source(),
-                            bikers[j].source(),
-                            *op
-                        );
-                        switch (comp)
-                        {
-                        case CGAL::LARGER:
-                            // all good, continue
-                            std::cout << "  " << bikers[i] << " beats " << bikers[j] << std::endl;
-                            break;
-                        case CGAL::SMALLER:
-                            // the biker is cut off
-                            std::cout << "  " << bikers[i] << " loses to " << bikers[j] << std::endl;
-                            is_cut_off = true;
-                            break;
-                        case CGAL::EQUAL:
-                            // same distance
-                            std::cout << "  " << bikers[i] << " equal " << bikers[j] << std::endl;
-                            if (ydiffs[i] < 0) is_cut_off = true;
-                            break;
-                        default:
-                            break;
-                        }
+                    switch (CGAL::compare_slope(bikers[i], bikers[j]))
+                    {
+                    case CGAL::SMALLER:
+                        std::cout << "  " << bikers[i] << " is smaller than " << bikers[j] << std::endl;
+                        break;
+                    case CGAL::LARGER:
+                        std::cout << "  " << bikers[i] << " is larger than " << bikers[j] << std::endl;
+                        break;
+                    case CGAL::EQUAL:
+                        std::cout << "  " << bikers[i] << " is equal than " << bikers[j] << std::endl;
+                        break;
+                    
+                    default:
+                        break;
                     }
                 }
             }

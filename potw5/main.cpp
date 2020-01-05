37b0650d429d6a17883e8ea0319ca10e4aa28ef6
Zak Cook <zakcook4@gmail.com>
2020-01-05 15:00:50 +0100

    40 points on Motorcycles

--- potw5/main.cpp
+++ potw5/main.cpp
@@ -6,9 +6,8 @@
 typedef CGAL::Exact_predicates_inexact_constructions_kernel IK;
 typedef CGAL::Exact_predicates_exact_constructions_kernel   EK;
 
-int compare_bikers(IK::Segment_2 &b1, IK::Segment_2 &b2)
-{
-    return b1.source().y() > b2.source().y();
+int compare_bikers(std::pair<IK::Ray_2, size_t> b1, std::pair<IK::Ray_2, size_t> b2) {
+    return b1.first.source().y() > b2.first.source().y();
 }
 
 int main(int argc, char const *argv[])
@@ -17,74 +16,90 @@ int main(int argc, char const *argv[])
     while (num_tests-- > 0)
     {
         int n; std::cin >> n;
-        std::vector<IK::Segment_2> bikers;
 
+        std::vector<std::pair<IK::Ray_2, size_t>> bikers;
         for (size_t i = 0; i < n; i++)
         {
             long y0, x1, y1; std::cin >> y0 >> x1 >> y1;
-            IK::Segment_2 biker(IK::Point_2(0, y0), IK::Point_2(x1, y1));
-            bikers.push_back(biker);
+            IK::Ray_2 biker(IK::Point_2(0, y0), IK::Point_2(x1, y1));
+            bikers.push_back(std::make_pair(biker, i));
         }
 
         std::sort(bikers.begin(), bikers.end(), compare_bikers);
 
-        std::vector<IK::FT> ydiffs;
-        for (auto &b : bikers) {
-            IK::FT ydiff = b.target().y() - b.source().y();
-            ydiffs.push_back(ydiff);
-            std::cout << "  " << b << " is going " << ydiff << std::endl;
-        }
+        std::vector<EK::FT> ydiffs;
+        for (auto &b : bikers)
+            ydiffs.push_back(b.first.point(1).y() - b.first.source().y());
 
-        std::vector<int> rides_forever_into_the_sunshine;
+        std::vector<size_t> sunshine;
 
         for (size_t i = 0; i < n; i++)
         {
+            size_t    index = bikers[i].second;
+            IK::Ray_2 biker = bikers[i].first;
 
-            std::vector<int> range;
-            if (ydiffs[i] > 0) // going up
-                for (int j = 0; j < i; j++)
-                    range.push_back(j);
-            else if (ydiffs[i] < 0) // going down
-                for (int j = i + 1; j < n; j++)
-                    range.push_back(j);
-            else {
-                std::cout << "  straight, ignoring..." << std::endl;
-                rides_forever_into_the_sunshine.push_back(i);
-                continue;
-            }
-            std::cout << "  range size is " << range.size() << std::endl;
+            // slope of the biker
+            EK::FT biker_slope = CGAL::abs(biker.direction().dy() / biker.direction().dx());
+
             bool is_cut_off = false;
-            for (int j : range) {
-                if (is_cut_off) break;
-                if (CGAL::do_intersect(bikers[i], bikers[j])) {
-                    switch (CGAL::compare_slope(bikers[i], bikers[j]))
+            if (ydiffs[i] < 0)
+            {
+                for (size_t j = i + 1; j < n && !is_cut_off; j++)
+                {
+                    IK::Ray_2 rival = bikers[j].first;
+                    if (CGAL::do_intersect(biker, rival))
                     {
-                    case CGAL::SMALLER:
-                        std::cout << "  " << bikers[i] << " is smaller than " << bikers[j] << std::endl;
-                        break;
-                    case CGAL::LARGER:
-                        std::cout << "  " << bikers[i] << " is larger than " << bikers[j] << std::endl;
-                        break;
-                    case CGAL::EQUAL:
-                        std::cout << "  " << bikers[i] << " is equal than " << bikers[j] << std::endl;
-                        break;
-                    
-                    default:
-                        break;
+                        if (ydiffs[j] <= 0)
+                        {
+                            // if there is an intersection and both are going downwards, the
+                            // biker has lost
+                            is_cut_off = true;
+                        }
+                        else 
+                        {
+                            // if same distance to point, the one with the flattest slope wins,
+                            // or the rival if they are equal (rival has right of way)
+                            EK::FT rival_slope = CGAL::abs(rival.direction().dy() / rival.direction().dx());
+                            if (rival_slope <= biker_slope)
+                                is_cut_off = true;
+                        }
                     }
                 }
             }
-            if (!is_cut_off) rides_forever_into_the_sunshine.push_back(i);
+            else if (ydiffs[i] > 0)
+            {
+                for (size_t j = 0; j < i && !is_cut_off; j++)
+                {
+                    IK::Ray_2 rival = bikers[j].first;
+                    if (CGAL::do_intersect(biker, rival))
+                    {
+                        if (ydiffs[j] >= 0)
+                        {
+                            // if there is an intersection and both are going downwards, the
+                            // biker has lost
+                            is_cut_off = true;
+                        }
+                        else 
+                        {
+                            // if same distance to point, the one with the flattest slope wins,
+                            // or the rival if they are equal (rival has right of way)
+                            EK::FT rival_slope = CGAL::abs(rival.direction().dy() / rival.direction().dx());
+                            if (rival_slope < biker_slope)
+                                is_cut_off = true;
+                        }
+                    }
+
+                }
+            }
+
+            if (!is_cut_off) sunshine.push_back(index);
         }
+
+        std::sort(sunshine.begin(), sunshine.end());
         
-        for (int i : rides_forever_into_the_sunshine)
-        {
-            std::cout << i <<  " ";
-        }
-        
+        for (size_t i : sunshine)
+            std::cout << i << " ";
         std::cout << std::endl;
-        
-        
     }
     
     return 0;

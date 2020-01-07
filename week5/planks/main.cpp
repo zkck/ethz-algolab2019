86f0059cadf07a708e2915d85cc706bbd4edc277
Zak Cook <zakcook4@gmail.com>
2020-01-07 13:09:29 +0100

    WIP on Planks

--- week5/planks/main.cpp
+++ week5/planks/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main(int argc, char const *argv[])
 {
@@ -6,11 +8,31 @@ int main(int argc, char const *argv[])
     while (num_tests-- > 0)
     {
         int n; std::cin >> n;
-        int l[n];
+        std::vector<int> lengths;
+        int total_perimeter = 0;
         for (size_t i = 0; i < n; i++)
         {
-            std::cin >> l[i];
+            int l; std::cin >> l;
+            lengths.push_back(l);
+            total_perimeter += l;
         }
+
+        std::sort(lengths.begin(), lengths.end());
+        int side_length = total_perimeter / 4;
+
+        for (size_t i = 0; i < n; i++)
+        {
+            int diff = side_length - lengths[i];
+            for (size_t j = i + 1; j < n; j++)
+            {
+                if (lengths[j] == diff)
+                {
+                    
+                }
+            }
+            
+        }
+        
 
         
         

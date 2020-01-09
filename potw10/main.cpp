e2abb2034c93117d8cadd43e66247b16b7ddf045
Zak Cook <zakcook4@gmail.com>
2020-01-09 11:20:44 +0100

    Attempt at Goldeneye

--- potw10/main.cpp
+++ potw10/main.cpp
@@ -1,7 +1,101 @@
 #include <iostream>
 
+// BFS
+#include <queue>
+
+// From CGAL docs
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <vector>
+
+typedef size_t info;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel         K;
+typedef CGAL::Triangulation_vertex_base_with_info_2<info, K>        Vb;
+typedef CGAL::Triangulation_data_structure_2<Vb>                    Tds;
+typedef CGAL::Delaunay_triangulation_2<K, Tds>                      Delaunay;
+typedef Delaunay::Point                                             Point;
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int num_tests; std::cin >> num_tests;
+    while (num_tests-- > 0)
+    {
+        int n, m, p; std::cin >> n >> m >> p;
+
+        std::vector<std::pair<Point, info>> jammers;
+        for (size_t i = 0; i < n; i++)
+        {
+            int x, y; std::cin >> x >> y;
+            jammers.push_back(std::make_pair(Point(x, y), i));
+        }
+
+        int p_squared = p * p;
+
+        Delaunay T;
+        T.insert(jammers.begin(), jammers.end());
+
+        for (size_t i = 0; i < m; i++)
+        {
+            int x0, y0, x1, y1; std::cin >> x0 >> y0 >> x1 >> y1;
+            Point s(x0, y0), t(x1, y1);
+
+            // BFS
+            // ===
+
+            std::vector<int> visited(n, false);
+            std::queue<Delaunay::Vertex_handle> Q;
+            
+            // source is the nearest vertex to start of mission
+            Delaunay::Vertex_handle src = T.nearest_vertex(s);
+            if (CGAL::squared_distance(s, src->point()) <= p / 4)
+            {
+                Q.push(src);
+                visited[src->info()] = true;
+            }
+
+            // destination is the nearest vertex to end of mission
+            Delaunay::Vertex_handle dst = T.nearest_vertex(t);
+            int success = CGAL::squared_distance(t, dst->point()) <= p / 4
+                ? -1    // unknown
+                :  0;   // unreachable
+
+            // std::cout << "BFS starting" << std::endl;
+
+            while (!Q.empty() && success < 0) {
+                const Delaunay::Vertex_handle u = Q.front();
+                Q.pop();
+
+                if (u->info() == dst->info()) {
+                    success = 1; // reachable
+                }
+
+                Delaunay::Vertex_circulator v = u->incident_vertices();
+                do
+                {
+                    int reachable = CGAL::squared_distance(u->point(), v->point()) <= p / 4;
+                    if (reachable && !visited[v->info()]) {
+                        Q.push(v);
+                        visited[v->info()] = true;
+                    }
+                } while (++v != u->incident_vertices());
+            }
+
+            // std::cout << "BFS done" << std::endl;
+
+            if (success == 1)
+                std::cout << "y";
+            else
+                std::cout << "n";
+
+            std::flush(std::cout);
+        }
+
+        std::cout << std::endl;
+        
+        
+    }
+    
     return 0;
 }

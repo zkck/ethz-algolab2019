c17145394daa57e77f915b2ef2469624617473f4
Zak Cook <zakcook4@gmail.com>
2020-01-11 15:00:53 +0100

    Cleanup

--- potw07/main.cpp
+++ potw07/main.cpp
@@ -18,22 +18,14 @@ int interval_scheduling(int separation_point) {
 
     for (auto &j : jedi) {
         std::tie(a, b) = j;
-
-        // two possible cases:
-        //   - separation_point < a && a <= m
-        //   - 0 < a && a < separation_point
         if (separation_point < a && a <= m) {
-            // can be considered as is
             adapted_jedi.push_back(std::make_pair(a, b));
         } else if (0 < a && a < separation_point) {
-            // has to be shifted
             adapted_jedi.push_back(std::make_pair(a + m, b + m));
         } else {
             throw std::runtime_error("invalid jedi");
         }
     }
-
-    // for (auto &j : adapted_jedi) printf("  %d, %d\n", j.first, j.second);
 
     int count = 0;
     int expanded = true;
@@ -46,12 +38,10 @@ int interval_scheduling(int separation_point) {
             if (a > pointer) {
                 expanded = true;
                 earliest_ft = std::min(earliest_ft, b);
-                // printf("    compatible, ft is %d\n", earliest_ft);
             }
         }
         if (expanded) {
             count++;
-            // printf("  pointer %d -> %d\n", pointer, earliest_ft);
             pointer = earliest_ft;
         }
     }
@@ -74,36 +64,6 @@ int main(int argc, char const *argv[])
                 jedi.push_back(std::make_pair(a, b + m));
         }
 
-        // std::cout << "n=" << n << " m=" << m << std::endl;
-
-        // // FIRST PHASE
-        // //
-        // // Build conflict map
-
-        // int a1, b1, a2, b2;
-        // for (size_t i = 0; i < n; i++) {
-        //     std::tie(a1, b1) = jedi[i];
-        //     std::vector<size_t> cs;
-        //     for (size_t j = 0; j < n; j++) {
-        //         if (j == i) continue;
-        //         std::tie(a2, b2) = jedi[j];
-        //         if (b1 > m) {
-        //             if (a2 + m <= b1 || b2 >= a1) {
-        //                 cs.push_back(j);
-        //             }
-        //         } else {
-        //             if (b1 >= a2 && b2 >= a1 || b2 >= a1 + m) {
-        //                 cs.push_back(j);
-        //             }
-        //         }
-        //     }
-        // }
-
-        // SECOND PHASE
-        //
-        // Check if we can use an uncovered segment, in which case it resembles a simple
-        // interval scheduling problem
-
         std::sort(jedi.begin(), jedi.end());
         int cover_start, cover_end; std::tie(cover_start, cover_end) = jedi[0];
         for (auto &j : jedi) {
@@ -112,13 +72,13 @@ int main(int argc, char const *argv[])
             cover_end = std::max(cover_end, b);
         }
 
-        // std::cout << "  cover_start=" << cover_start << " cover_end=" << cover_end << std::endl;
         int separation_point = -1;
         if (cover_start + m - cover_end > 1) separation_point = (cover_end + 1) % m;
-        // std::cout << "  separation=" << separation_point << std::endl;
 
         if (separation_point >= 0)
             std::cout << interval_scheduling(separation_point) << std::endl;
+        else;
+            // TODO
     }
 
     return 0;

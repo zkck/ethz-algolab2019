2994099f8ea4739971c702c8eea1265f5d5399e3
Zak Cook <zakcook4@gmail.com>
2020-01-14 14:32:54 +0100

    WIP, coded the recursion to find the subsets

--- week05/light_at_the_museum/main.cpp
+++ week05/light_at_the_museum/main.cpp
@@ -1,31 +1,62 @@
 #include <iostream>
 #include <vector>
 
-int num_sums(int brightness, int off[], int on[], int N, std::vector<size_t> &subsets)
-{
-    for (size_t ss = 0; ss < 1<<N; ss++) {
-        long ss_sum;
-        for (size_t i = 0; i < N; ++i) {
-            if (1<<i & ss) ss += on[i];
-            else           ss += off[i];
-        }
-        if (ss_sum = brightness) subsets.push_back(ss);
+void binary_print(size_t i, int N) {
+    for (size_t idx = 0; idx < N; ++idx) {
+        if (i & 1<<idx) std::cout << '1';
+        else            std::cout << '0';
     }
-    return subsets.size();
+    std::cout << std::endl;
 }
 
-int disjoint_rec(std::vector<size_t> subsets[], int i, int M, size_t sofar, std::vector<size_t> disjoint) {
-    if (i == M) {
-        disjoint.push_back(sofar);
-        return 1;
-    }
-    int count = 0;
-    for (size_t ss : subsets[i]) {
-        if ( !(ss & sofar)) {
-            count += disjoint_rec(subsets, i + 1, M, sofar | ss, disjoint);
+int num_sums(int brightness,
+    int off[],
+    int on[],
+    int i,
+    int N,
+    int subset,
+    int mask,
+    std::vector<int> &subsets,
+    std::vector<int> &masks)
+{
+    if (i == N) {
+        if (brightness == 0) {
+            subsets.push_back(subset);
+            masks.push_back(mask);
+            return 1;
+        } else {
+            return 0;
         }
     }
-    return count;
+    int result = 0;
+    if (on[i] == 0 && off[i] == 0)
+        result += num_sums(brightness, off, on, i + 1, N, subset, mask, subsets, masks);
+    else {
+        result += num_sums(brightness - off[i],
+            off,
+            on,
+            i + 1,
+            N,
+            subset,         // does not change since we are adding an off (0) switch
+            mask | (1<<i),  // changes because we are "setting" this switch
+            subsets,
+            masks);
+        result += num_sums(brightness - on[i],
+            off,
+            on,
+            i + 1,
+            N,
+            subset | (1<<i),    // changes because we have the switch on
+            mask | (1<<i),      // changes because we are "setting" this switch
+            subsets,
+            masks);
+    }
+    return result;
+}
+
+int num_compatible(std::vector<int> subsets[], std::vector<int> masks[], int i, int M, int mask, int subset) {
+    // TODO
+    return 0;
 }
 
 int main(int argc, char const *argv[])
@@ -39,33 +70,18 @@ int main(int argc, char const *argv[])
         {
             std::cin >> b[i];
         }
-        int on[N], off[N];
+        int on[M][N], off[M][N];
         for (size_t i = 0; i < N; i++)
-        {
-            std::cin >> on[i] >> off[i];
-        }
-        std::vector<size_t> subsets[M];
+            for (size_t j = 0; j < M; j++)
+                std::cin >> on[j][i] >> off[j][i];
+
+        std::vector<int> subsets[M];
+        std::vector<int> masks[M];
         for (size_t i = 0; i < M; i++) {
-            num_sums(b[i], off, on, N, subsets[i]);
-        }
-        std::vector<size_t> disjoint_subsets;
-        disjoint_rec(subsets, 0, M, 0, disjoint_subsets);
-
-        // Find minimum
-        size_t min_dss = 0;
-        int min_count = N;
-        for (size_t dss : disjoint_subsets)
-        {
-            int count = 0;
-            for (size_t i = 0; i < M; i++)
-                if (1<<i & dss) count++;
-            if (count < min_count) {
-                min_count = count;
-                min_dss = dss;
-            }
+            num_sums(b[i], off[i], on[i], 0, N, 0, 0, subsets[i], masks[i]);
         }
 
-        std::cout << min_count << std::endl;
+
 
     }
 

9fa772dbb72bc8e37f723fbf960c9e91c6a01edf
Zak Cook <zakcook4@gmail.com>
2020-01-15 13:28:45 +0100

    100 points on Magician and the Coin

--- week12/magician_coin/main.cpp
+++ week12/magician_coin/main.cpp
@@ -5,9 +5,12 @@
 int n, m;
 std::vector<double> probabilities;
 
+std::vector<std::vector<double>> memo;
+
 double probability_optimal_strategy(int budget, int day) {
     if (budget >= m) return 1;
     if (day == n)    return 0;
+    if (memo[day][budget] >= 0) return memo[day][budget];
     double best = 0;
     for (int b = 0; b <= budget; b++)
     {
@@ -16,6 +19,7 @@ double probability_optimal_strategy(int budget, int day) {
         prob += (1 - probabilities[day]) * probability_optimal_strategy(budget - b, day + 1);
         best = std::max(best, prob);
     }
+    memo[day][budget] = best;
     return best;
 }
 
@@ -31,6 +35,9 @@ int main(int argc, char const *argv[])
             double p; std::cin >> p;
             probabilities.push_back(p);
         }
+
+        memo.clear();
+        memo.resize(n, std::vector<double>(m, -1));
 
         std::cout << std::fixed << std::setprecision(5);
         std::cout << probability_optimal_strategy(k, 0) << std::endl;

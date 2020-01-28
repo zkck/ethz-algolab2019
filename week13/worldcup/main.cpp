7ed49b681cca9055b263c2007e73c42bb8a302b9
Zak Cook <zakcook4@gmail.com>
2020-01-28 21:59:31 +0100

    50 points on WC, trying for 100

--- week13/worldcup/main.cpp
+++ week13/worldcup/main.cpp
@@ -6,7 +6,7 @@
 #include <CGAL/Gmpq.h>
 
 // choose input type (input coefficients must fit)
-typedef double IT;
+typedef CGAL::Gmpq IT;
 // choose exact type for solver (CGAL::Gmpz or CGAL::Gmpq)
 typedef CGAL::Gmpq ET;
 
@@ -14,21 +14,42 @@ typedef CGAL::Gmpq ET;
 typedef CGAL::Quadratic_program<IT> Program;
 typedef CGAL::Quadratic_program_solution<ET> Solution;
 
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/Triangulation_vertex_base_with_info_2.h>
+#include <vector>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel         K;
+typedef CGAL::Triangulation_vertex_base_with_info_2<unsigned, K>    Vb;
+typedef CGAL::Triangulation_data_structure_2<Vb>                    Tds;
+typedef CGAL::Delaunay_triangulation_2<K, Tds>                      Delaunay;
+typedef Delaunay::Point                                             Point;
+
+
 struct warehouse {
     int x;
     int y;
-    int s;
-    int a;
+    IT s;
+    IT a;
 };
 
 struct stadium {
     int x;
     int y;
-    int d;
-    int u;
+    IT d;
+    IT u;
 };
 
-int var(int wh, int st, int m) {
+int n, m, c;
+
+double floor_to_double(const ET x) {
+  double a = std::floor(CGAL::to_double(x));
+  while (a > x) a -= 1;
+  while (a+1 <= x) a += 1;
+  return a;
+}
+
+
+int quantity(int wh, int st) {
     return wh * m + st;
 }
 
@@ -36,69 +57,133 @@ int main(int argc, char const *argv[])
 {
     int num_tests; std::cin >> num_tests;
     while (num_tests-- > 0) {
-        int n, m, c; std::cin >> n >> m >> c;
+        // n : number of warehouses
+        // m : number of stadiums
+        // c : number of contour lines
+        std::cin >> n >> m >> c;
 
         std::vector<struct warehouse> warehouses;
         for (size_t i = 0; i < n; i++)
         {
             struct warehouse warehouse;
+            // x : position
+            // y : position
+            // s : supply in liters
+            // a : percentage of alcohol
             std::cin >> warehouse.x >> warehouse.y >> warehouse.s >> warehouse.a;
             warehouses.push_back(warehouse);
         }
+
         std::vector<struct stadium> stadiums;
         for (size_t i = 0; i < m; i++)
         {
             struct stadium stadium;
+            // x : position
+            // y : position
+            // d : demand in liters
+            // u : upper bound for liters of alcohol
             std::cin >> stadium.x >> stadium.y >> stadium.d >> stadium.u;
             stadiums.push_back(stadium);
         }
-        int revenues[n][m];
-        for (size_t i = 0; i < n; i++)
-        {
-            for (size_t j = 0; j < m; j++)
-            {
-                std::cin >> revenues[i][j];
+
+
+        IT r[n][m];
+        for (size_t i = 0; i < n; i++) {
+            for (size_t j = 0; j < m; j++) {
+                // r[i][j] : revenue for moving a liter from warehouse i to stadium j
+                std::cin >> r[i][j];
             }
         }
+
+        // Fourth group of testsets states that at most 100 contour lines contain at least one
+        // warehouse in their interior.
+        //
+        // This suggest triangulating the stadiums/warehouses, and checking if the contour
+        // line is in range of any stadiums or warehouses.
+
+        std::vector<int> num_conflicts(m + n, 0);
+
+        size_t index = 0;
+        std::vector<std::pair<Point, size_t>> points;
+        for (auto &stadium : stadiums)
+            points.push_back(std::make_pair(Point(stadium.x, stadium.y), index++));
+        for (auto &warehouse : warehouses)
+            points.push_back(std::make_pair(Point(warehouse.x, warehouse.y), index++));
+
+        Delaunay T;
+        T.insert(points.begin(), points.end());
+
         for (size_t i = 0; i < c; i++)
         {
+            // x : position
+            // y : position
+            // r : radius
             int x, y, r; std::cin >> x >> y >> r;
+
+            Point p(x, y);
+            auto v = T.nearest_vertex(p);
+
+            if (CGAL::squared_distance(p, v->point()) > r * r)
+                continue;
+
+            index = 0;
+
+            // Check stadiums conflicts
+            for (auto &st : stadiums)
+                num_conflicts[index++] += CGAL::squared_distance(Point(st.x, st.y), p) >= r * r
+                    ? 0
+                    : 1;
+
+            // Check warehouse conflicts
+            for (auto &wh : warehouses)
+                num_conflicts[index++] += CGAL::squared_distance(Point(wh.x, wh.y), p) >= r * r
+                    ? 0
+                    : 1;
         }
 
         Program lp (CGAL::SMALLER, true, 0, false, 0);
         int num_rows = 0;
 
-        // for each stadium, set alcohol's upper limit
+        // STEP 1
+        //
+        // For each stadium, set alcohol from all warehouses times their alcohol percentage
+        // to less than the upper bound.
+
         for (size_t st = 0; st < m; st++) {
             for (size_t wh = 0; wh < n; wh++) {
-                lp.set_a(var(wh, st, m), st, warehouses[wh].a / 100.0);
+                lp.set_a(quantity(wh, st), st, warehouses[wh].a / 100.0);
             }
             lp.set_b(st, stadiums[st].u);
         }
 
         num_rows += m;
 
-        // for each warehouse, limit the supply to all stadiums
-        for (size_t wh = 0; wh < n; wh++)
-        {
-            for (size_t st = 0; st < m; st++)
-            {
-                lp.set_a(var(wh, st, m), num_rows + wh, 1);
+        // STEP 2
+        //
+        // For each warehouse, limit the sum of the supply to all stadiums to that warehouse's
+        // supply.
+
+        for (size_t wh = 0; wh < n; wh++) {
+            for (size_t st = 0; st < m; st++) {
+                lp.set_a(quantity(wh, st), num_rows + wh, 1);
             }
             lp.set_b(num_rows + wh, warehouses[wh].s);
         }
 
         num_rows += n;
 
-        // upper bound and lower bound on the demand of the stadiums
+        // STEP 3
+        //
+        // The sum of quantities to a stadium must be equal to the demand.
+
         for (size_t st = 0; st < m; st++)
         {
             int upper = st * 2;
             int lower = st * 2 + 1;
             for (size_t wh = 0; wh < n; wh++)
             {
-                lp.set_a(var(wh, st, m), num_rows + upper,  1);
-                lp.set_a(var(wh, st, m), num_rows + lower, -1);
+                lp.set_a(quantity(wh, st), num_rows + upper,  1);
+                lp.set_a(quantity(wh, st), num_rows + lower, -1);
             }
             lp.set_b(num_rows + upper,  stadiums[st].d);
             lp.set_b(num_rows + lower, -stadiums[st].d);
@@ -106,36 +191,28 @@ int main(int argc, char const *argv[])
 
         num_rows += 2 * m;
 
-        for (size_t wh = 0; wh < n; wh++)
-        {
-            for (size_t st = 0; st < m; st++)
-            {
-                lp.set_c(var(wh, st, m), -revenues[wh][st]);
+        // FINAL STEP (objective function)
+        //
+        // Multiply each dispatched quantity with the revenue (must be negated since LP
+        // minimizes).
+
+        for (size_t wh = 0; wh < n; wh++) {
+            for (size_t st = 0; st < m; st++) {
+                int num_confs =  num_conflicts[m + wh] + num_conflicts[st];
+                lp.set_c(quantity(wh, st), -r[wh][st] - (num_confs / 100.0));
             }
         }
 
-
-
+        // SOLUTION
 
         Solution s = CGAL::solve_linear_program(lp, ET());
         if (s.is_optimal()) {
-            std::cout << s.objective_value() << std::endl;
+            std::cout << (int) std::floor(-CGAL::to_double(s.objective_value())) << std::endl;
         } else if (s.is_unbounded()) {
             std::cout << "nope" << std::endl;
         } else if (s.is_infeasible()) {
             std::cout << "RIOT!" << std::endl;
         }
-
-
-
-
-
-
-
-
-
-
-
     }
     return 0;
 }

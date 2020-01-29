62236c8194f2f177f5d04cc67377b11124ddcab4
Zak Cook <zakcook4@gmail.com>
2020-01-29 23:19:11 +0100

    Split and list for LATM 70 pts

--- week05/light_at_the_museum/main.cpp
+++ week05/light_at_the_museum/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <algorithm>
 
 int binary_count(int i, int N) {
     int count = 0;
@@ -25,28 +27,78 @@ int main(int argc, char const *argv[])
             for (size_t j = 0; j < M; j++)
                 std::cin >> on[j][i] >> off[j][i];
 
-        std::vector<int> subsets;
-        for (int subset = 0; subset < 1<<N; subset++) {
-            int correct = true;
-            for (size_t i = 0; i < M && correct; i++) {
+        // IDEA
+        //
+        // For each room find the satisfying subsets, using split and list. Then move to
+        // the next room and from those satisfying subsets,
+
+        // high     |      low
+        //
+        // ---L2--- | ---L1---
+
+        // L1 is first N / 2 numbers
+        const int L1 = N / 2;
+
+        // L2 is remaining numbers
+        const int L2 = N - L1;
+
+        std::vector<int> satisfying_subsets;
+
+        if (M == 1) {
+
+            size_t i = 0;
+
+            // Section 1: create the set to compare to
+            std::multimap<long, int> L2set;
+            for (int subset = 0; subset < 1<<L2; subset++) {
                 int sum = 0;
-                for (size_t j = 0; j < N && sum <= b[i]; j++) {
+                for (size_t j = 0; j < L2 && sum <= b[i]; j++) {
+                    if (subset & 1<<j) sum += on[i][j + L1];
+                    else               sum += off[i][j + L1];
+                }
+                if (sum <= b[i])
+                    L2set.insert(std::make_pair(sum, subset << L1));
+            }
+
+            // Section 2
+            for (int subset = 0; subset < 1<<L1; subset++) {
+                int sum = 0;
+                for (size_t j = 0; j < L1 && sum <= b[i]; j++) {
                     if (subset & 1<<j) sum += on[i][j];
                     else               sum += off[i][j];
                 }
-                if (sum != b[i]) correct = false;
+                if (sum <= b[i]) {
+                    auto range = L2set.equal_range(b[i] - sum);
+                    for (auto it = range.first; it != range.second; it++) {
+                        satisfying_subsets.push_back(subset | it->second);
+                    }
+                }
             }
-            if (correct) subsets.push_back(subset);
+
+        } else {
+
+            for (int subset = 0; subset < 1<<N; subset++) {
+                int correct = true;
+                for (size_t i = 0; i < M && correct; i++) {
+                    int sum = 0;
+                    for (size_t j = 0; j < N && sum <= b[i]; j++) {
+                        if (subset & 1<<j) sum += on[i][j];
+                        else               sum += off[i][j];
+                    }
+                    if (sum != b[i]) correct = false;
+                }
+                if (correct) satisfying_subsets.push_back(subset);
+            }
         }
 
         int min_length = N;
-        for (int ss : subsets) {
+        for (int ss : satisfying_subsets) {
             int count = binary_count(ss, N);
             if (count < min_length)
                 min_length = count;
         }
 
-        if (subsets.empty())
+        if (satisfying_subsets.empty())
             std::cout << "impossible" << std::endl;
         else
             std::cout << min_length << std::endl;

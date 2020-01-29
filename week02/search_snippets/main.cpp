a94a10963c0b58f408e937d36d53ec874db98a46
Zak Cook <zakcook4@gmail.com>
2020-01-29 15:27:49 +0100

    Search snippets 100

--- week02/search_snippets/main.cpp
+++ week02/search_snippets/main.cpp
@@ -1,40 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
-std::vector<std::vector<int>> memo;
-std::vector<std::vector<int>> p;
-
-int earliest_finish(int i);
-int earliest_finish(int i, int j);
-
-int earliest_finish(int i)
-{
-    int result = std::numeric_limits<int>::max();
-    for (size_t j = 0; j < p[i].size(); j++)
-    {
-        result = std::min(result, earliest_finish(i, j) - p[i][j]);
-    }
-    return result;
-}
-
-int earliest_finish(int i, int j)
-{
-    if (memo[i][j] >= 0) return memo[i][j];
-    if (i == p.size() - 1) return p[i][j];
-
-    int result = std::numeric_limits<int>::max();
-
-    auto upper = std::upper_bound(p[i + 1].begin(), p[i + 1].end(), p[i][j]);
-    if (upper != p[i + 1].end())
-        result = std::min(result, earliest_finish(i + 1, upper - p[i + 1].begin()));
-
-    memo[i][j] = result;
-    return result;
-}
 
 int main(int argc, char const *argv[])
 {
+    std::ios_base::sync_with_stdio(false);
     int num_tests; std::cin >> num_tests;
     while (num_tests-- > 0)
     {
@@ -45,24 +17,50 @@ int main(int argc, char const *argv[])
             std::cin >> m[i];
         }
 
-        memo.clear();
-        memo.resize(n);
+        // IDEA
+        //
+        // Keep a sorted list of positions
 
-        p.clear();
-        p.resize(n);
+        std::multimap<int, size_t> positions;
 
+        std::vector<int> p[n];
         for (size_t i = 0; i < n; i++) {
+            p[i].resize(m[i]);
             for (size_t j = 0; j < m[i]; j++) {
-                int p_i_j; std::cin >> p_i_j;
-                p[i].push_back(p_i_j);
+                std::cin >> p[i][j];
+                positions.insert(std::make_pair(p[i][j], i));
             }
-            std::sort(p[i].begin(), p[i].end());
-            memo[i].resize(m[i], -1);
         }
 
-        std::cout << earliest_finish(0) << std::endl;
+        std::vector<int> occurrences(n, 0);
 
+        int min = std::numeric_limits<int>::max();
 
+        auto l = positions.begin();
+        auto r = positions.begin();
+        for (int num_distinct = 0; r != positions.end(); ++r) {
+
+            // new word
+            if (occurrences[r->second]++ == 0)
+                num_distinct++;
+
+            // increasing section
+            while (num_distinct < n) {
+                if (occurrences[(++r)->second]++ == 0)
+                    num_distinct++;
+            }
+
+            // decreasing section
+            while (occurrences[l->second] > 1) {
+                occurrences[(l++)->second]--;
+            }
+
+            if (num_distinct == n) {
+                min = std::min(min, r->first - l->first + 1);
+            }
+        } while (r != positions.end());
+
+        std::cout << min << std::endl;
     }
 
     return 0;
